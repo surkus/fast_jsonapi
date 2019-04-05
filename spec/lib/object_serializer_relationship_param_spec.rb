6cde627c4df130e47476c9bfeb1eb3e06ef6f891
Adam Braman <glyoko1@gmail.com>
2019-04-05 13:19:13 -0700

    Add test for Relationship#record_type

--- spec/lib/object_serializer_relationship_param_spec.rb
+++ spec/lib/object_serializer_relationship_param_spec.rb
@@ -5,6 +5,8 @@ describe FastJsonapi::ObjectSerializer do
 
   context "params option" do
     let(:hash) { serializer.serializable_hash }
+    let(:serializer) { MovieSerializer.new(movie, params: params) }
+    let(:params) { {authorized: true} }
 
     before(:context) do
       class MovieSerializer
@@ -16,19 +18,18 @@ describe FastJsonapi::ObjectSerializer do
           movie.actors.map(&:agency)[0] if params[:authorized]
         end
 
-        belongs_to :secondary_agency do |movie|
+        belongs_to :secondary_agency, serializer: AgencySerializer do |movie|
           movie.actors.map(&:agency)[1]
+        end
+
+        belongs_to :tertiary_agency, record_type: :custom_agency_type do |movie|
+          movie.actors.last.agency
         end
       end
     end
 
-    context "passing params to the serializer" do
-      let(:params) { {authorized: true} }
-      let(:options_with_params) { {params: params} }
-
+    describe "passing params to the serializer" do
       context "with a single record" do
-        let(:serializer) { MovieSerializer.new(movie, options_with_params) }
-
         it "handles relationships that use params" do
           ids = hash[:data][:relationships][:agencies][:data].map{|a| a[:id]}
           ids.map!(&:to_i)
@@ -42,8 +43,7 @@ describe FastJsonapi::ObjectSerializer do
 
       context "with a list of records" do
         let(:movies) { build_movies(3) }
-        let(:params) { {authorized: true} }
-        let(:serializer) { MovieSerializer.new(movies, options_with_params) }
+        let(:serializer) { MovieSerializer.new(movies, params: params) }
 
         it "handles relationship params when passing params to a list of resources" do
           relationships_hashes = hash[:data].map{|a| a[:relationships][:agencies][:data]}.uniq.flatten
@@ -56,6 +56,47 @@ describe FastJsonapi::ObjectSerializer do
         it "handles relationships without params" do
           uniq_count = hash[:data].map{|a| a[:relationships][:secondary_agency] }.uniq.count
           expect(uniq_count).to eq 1
+        end
+      end
+    end
+
+    describe '#record_type' do
+      let(:relationship) { MovieSerializer.relationships_to_serialize[relationship_name] }
+
+      context 'without any options' do
+        let(:relationship_name) { :primary_agency }
+        it 'infers record_type from relation name' do
+          expect(relationship.record_type).to eq :primary_agency
+        end
+      end
+
+      context 'with serializer option' do
+        let(:relationship_name) { :secondary_agency }
+        it 'uses type of given serializer' do
+          expect(relationship.record_type).to eq :agency
+        end
+      end
+
+      context 'with record_type option' do
+        let(:relationship_name) { :tertiary_agency }
+        it 'uses record_type option' do
+          expect(relationship.record_type).to eq :custom_agency_type
+        end
+      end
+
+      context 'with pluralize_type true' do
+        let(:relationship_name) { :secondary_agency }
+
+        before(:context) do
+          AgencySerializer.pluralize_type true
+        end
+
+        after(:context) do
+          AgencySerializer.pluralize_type nil
+        end
+
+        it 'uses record_type option' do
+          expect(relationship.record_type).to eq :agencies
         end
       end
     end
